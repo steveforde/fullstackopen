7fedc5b37f0a20bd3a9fb0769ccbb892ec815715
Steve Forde <sforde08@gmail.com>
2026-03-09 15:08:38 +0000

    unicafe-step4 added no feedback

---
+++ Task-1/unicafe-step4/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const total = good + neutral + bad;
+
+  // 1. If there is no feedback, show a message
+  if (total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  // 2. OTHERWISE: show the full table
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {((good - bad) / total).toFixed(13)}</p>
+      <p>positive {((good / total) * 100).toFixed(13)} %</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      {/* This is the "Dry" way—passing data to your component */}
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
