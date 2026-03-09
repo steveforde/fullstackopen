321b8f4ab2b03bc3efe23bcfe697dc0e9e982a1d
Steve Forde <sforde08@gmail.com>
2026-03-09 14:52:09 +0000

    unicafe-step3 seperate statistics

--- Task-1/unicafe-step3/src/App.jsx
+++ Task-1/unicafe-step3/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const total = good + neutral + bad;
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {total === 0 ? 0 : ((good - bad) / total).toFixed(13)}</p>
+      <p>positive {total === 0 ? 0 : ((good / total) * 100).toFixed(13)} %</p>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const total = good + neutral + bad;
 
   return (
     <div>
@@ -14,14 +28,8 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>Statistics</h1>
-
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {total === 0 ? 0 : ((good - bad) / total).toFixed(13)}</p>
-      <p>positive {total === 0 ? 0 : ((good / total) * 100).toFixed(13)} %</p>
+      {/* This is the "Dry" way—passing data to your component */}
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
