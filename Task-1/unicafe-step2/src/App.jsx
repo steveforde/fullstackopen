4c505b58ffaf598b82a2253b1130afb9ccd23d91
Steve Forde <sforde08@gmail.com>
2026-03-13 14:14:12 +0000

    updated layout

--- Task-1/unicafe-step2/src/App.jsx
+++ Task-1/unicafe-step2/src/App.jsx
@@ -13,15 +13,13 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-
       <h1>Statistics</h1>
-
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {total}</p>
+      <p>All {total}</p>
       <p>average {total === 0 ? 0 : ((good - bad) / total).toFixed(13)}</p>
-      <p>positive {total === 0 ? 0 : ((good / total) * 100).toFixed(13)} %</p>
+      <p>positive {total === 0 ? 0 : ((good / total) * 100).toFixed(13)}%</p>
     </div>
   );
 };
