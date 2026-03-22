2cfda930a9d6e9e50b567615a29a14c0bdb5fbea
Steve Forde <sforde08@gmail.com>
2026-03-22 09:26:57 +0000

    cleanup

--- Task-3/phonebook-backend-step9/index.js
+++ Task-3/phonebook-backend-step9/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const morgan = require("morgan"); // Don't forget this!
-const cors = require("cors");
 const app = express();
 
 app.use(express.static("dist"));
-app.use(cors());
 app.use(express.json());
 
 // Morgan configuration
